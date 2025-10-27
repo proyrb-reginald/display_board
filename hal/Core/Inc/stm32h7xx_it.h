b6bed454d53627244994ed9be87ddd5228ff1d83
proyrb <proyrb@yeah.net>
2025-10-27 17:52:07 +0800

    编译通过，未上机验证

--- hal/Core/Inc/stm32h7xx_it.h
+++ hal/Core/Inc/stm32h7xx_it.h
@@ -52,7 +52,6 @@ void BusFault_Handler(void);
 void UsageFault_Handler(void);
 void SVC_Handler(void);
 void DebugMon_Handler(void);
-void PendSV_Handler(void);
 void TIM1_UP_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
--- hal/Core/Src/stm32h7xx_it.c
+++ hal/Core/Src/stm32h7xx_it.c
@@ -151,19 +151,6 @@ void DebugMon_Handler(void)
   /* USER CODE END DebugMonitor_IRQn 1 */
 }
 
-/**
-  * @brief This function handles Pendable request for system service.
-  */
-void PendSV_Handler(void)
-{
-  /* USER CODE BEGIN PendSV_IRQn 0 */
-
-  /* USER CODE END PendSV_IRQn 0 */
-  /* USER CODE BEGIN PendSV_IRQn 1 */
-
-  /* USER CODE END PendSV_IRQn 1 */
-}
-
 /******************************************************************************/
 /* STM32H7xx Peripheral Interrupt Handlers                                    */
 /* Add here the Interrupt Handlers for the used peripherals.                  */
